354bd1ae47f5d3eb258cf359581d60e159375f44
Thiago <thiagoodemedeiros@gmail.com>
2023-10-06 18:20:19 -0300

    Buttons and JS

--- script/index.js
+++ script/index.js
@@ -6,3 +6,33 @@ const members = [
   { name: 'Rocket Raccoon', id: 'rocket' },
   { name: 'I am Groot', id: 'groot' },
 ]
+
+let activeMember = 0
+console.log(activeMember)
+
+const images = document.getElementById('images')
+
+function changeStatusButtons() {
+  const prev = document.getElementById('button_prev')
+  const isFirst = activeMember === 0
+  prev.removeAttribute("disabled") != isFirst
+  prev.disabled = isFirst
+
+  
+  const next = document.getElementById('button_next')
+  const isLast = activeMember === members.length - 1
+  next.disabled = isLast
+
+}
+
+function navigationMember(direction){
+  activeMember += direction
+
+  const member = members[activeMember]
+
+  console.log(activeMember, member)
+
+  images.style.transform = `translateY(${-100 * activeMember}vh)` //muda os personagens com translate
+
+  changeStatusButtons()
+}
